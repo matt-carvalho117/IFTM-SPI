cb97ce30f9e5a84c0587ea4c659be6af41526b53
Ghost <matheus.roldao@estudante.iftm.edu.br>
2026-08-24 13:12:23 -0300

    Lista poo exe 7

--- 2°Periodo/POO1/Listas/main.java
+++ 2°Periodo/POO1/Listas/main.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class main {
 
-    //EX1
+    // EX1
     public static void exe1() {
         int x, y;
 
@@ -32,7 +32,7 @@ public class main {
         }
     }
 
-    //EX2
+    // EX2
     public static void exe2() {
         int a, b, c;
         double x1, x2;
@@ -56,7 +56,7 @@ public class main {
         JOptionPane.showMessageDialog(null, "As raizes são\nX1 =  " + x1 + "\nX2 = " + x2);
     }
 
-    //EX3
+    // EX3
     public static void exe3() {
         int n1, n2, media;
 
@@ -76,7 +76,7 @@ public class main {
         JOptionPane.showMessageDialog(null, "A média é: " + media);
     }
 
-    //EX4
+    // EX4
     public static void exe4() {
         int l1, l2, l3;
 
@@ -88,11 +88,9 @@ public class main {
 
             if (l1 == l2 && l2 == l3) {
                 exibir4(1);
-            } 
-            else if (l1 == l2 || l2 == l3 || l3 == l1) {
+            } else if (l1 == l2 || l2 == l3 || l3 == l1) {
                 exibir4(2);
-            }
-            else if (l1 != l2 && l2 != l3 && l3 != l1) {
+            } else if (l1 != l2 && l2 != l3 && l3 != l1) {
                 exibir4(3);
             }
         } else {
@@ -113,44 +111,156 @@ public class main {
         }
     }
 
-    
-    //EX5
-    public static void exe5(){
-       int soma = 0;
-       int somaNeg = 0;
-       int somaPos = 0;
-       
-       int n = ler5();
-       
-       while(n != 0){
-           soma += n;
-           
-           if(soma >= 0){
-               somaPos++;
-           }
-           else{
-               somaNeg++;
-           }
-           n = ler5();
-       }
-       
-       exibir5(somaPos, somaNeg);
-      
+    // EX5
+    public static void exe5() {
+        int soma = 0;
+        int somaNeg = 0;
+        int somaPos = 0;
+
+        int n = ler5();
+
+        while (n != 0) {
+            soma += n;
+
+            if (soma >= 0) {
+                somaPos++;
+            } else {
+                somaNeg++;
+            }
+            n = ler5();
+        }
+
+        exibir5(somaPos, somaNeg);
+
     }
-    
-    public static int ler5(){
+
+    public static int ler5() {
         return Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número:"));
     }
-   
-    public static void exibir5(int somaPos, int somaNeg){
+
+    public static void exibir5(int somaPos, int somaNeg) {
         JOptionPane.showMessageDialog(null, "Somas positivas = " + somaPos + "\nSomas negativas = " + somaNeg);
     }
-    
-    
+
+    // EX06
+    public static void exe6() {
+        int n = ler6();
+        exibir6(n);
+        char op = lerOp();
+
+        while (op == 's' || op == 'S') {
+            n = ler6();
+            exibir6(n);
+            op = lerOp();
+        }
+    }
+
+    public static int ler6() {
+        return Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número para exibir seus fatores:"));
+    }
+
+    public static char lerOp() {
+        return JOptionPane.showInputDialog(null, "Deseja digitar novamente?").charAt(0);
+    }
+
+    public static String fatores6(int n) {
+        String results = "";
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                results += i + " ";
+            }
+        }
+        return results;
+    }
+
+    public static void exibir6(int n) {
+        JOptionPane.showMessageDialog(null, "Os fatores de " + n + " são: " + fatores6(n));
+    }
+
+    // EXE07
+    public static void exe7() {
+        int n = ler7();
+        int qtdPares = 0;
+        int somaPares = 0;
+        int qtdImpares = 0;
+        int somaImpares = 0;
+        int qtdPositivos = 0;
+        int somaPositivos = 0;
+        int qtdNegativos = 0;
+        int somaNegativos = 0;
+
+        while (n != 0) {
+            if(n % 2 == 0){
+                qtdPares++;
+                somaPares += n;
+            } else {
+                qtdImpares++;
+                somaImpares += n;
+            }
+            if(n > 0){
+                qtdPositivos++;
+                somaPositivos += n;
+            } else{
+                qtdNegativos++;
+                somaNegativos += n;
+            }
+            n = ler7();
+        }
+        exibir7(qtdPares, somaPares, qtdImpares, somaImpares, qtdPositivos, somaPositivos, qtdNegativos, somaNegativos);
+    }
+
+    public static int ler7() {
+        return Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número de cada vez:"));
+    }
+    public static void exibir7(int qtdPares, int somaPares, int qtdImpares, int somaImpares, int qtdPositivos, int somaPositivos, int qtdNegativos, int somaNegativos) {
+        JOptionPane.showMessageDialog(null, "Quantidade de números pares: " + qtdPares + "\nSoma dos números pares: " + somaPares + "\nQuantidade de números ímpares: " + qtdImpares + "\nSoma dos números ímpares: " + somaImpares + "\nQuantidade de números positivos: " + qtdPositivos + "\nSoma dos números positivos: " + somaPositivos + "\nQuantidade de números negativos: " + qtdNegativos + "\nSoma dos números negativos: " + somaNegativos);
+    }
+
+    // EXE09
+    public static void exe9() {
+        int n = ler9();
+        exibir9(n);
+    }
+
+    public static int calcularFatorial9(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * calcularFatorial9(n - 1);
+        }
+    }
+
+    public static int ler9() {
+        return Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número para exibir seu fatorial:"));
+    }
+
+    public static void exibir9(int n) {
+        JOptionPane.showMessageDialog(null, "O fatorial de " + n + " é: " + calcularFatorial9(n));
+    }
+
     public static void main(String[] args) {
         int op;
         do {
-            op = Integer.parseInt(JOptionPane.showInputDialog("LISTA - 01   POO\n\n1- Comparar Números\n2- Equação do Segundo Grau\n3- Média 2 números\n4- Tipos de Triângulos\n5- Somar positivos e negativos\n17- Sair\n\nSelecione uma opcão:"));
+            op = Integer.parseInt(JOptionPane.showInputDialog(
+                    "            LISTA - 01 POO\n\n" +
+                            "1 - Comparar Números\n" +
+                            "2 - Equação do Segundo Grau\n" +
+                            "3 - Média de 2 Números\n" +
+                            "4 - Tipos de Triângulos\n" +
+                            "5 - Somar Positivos e Negativos\n" +
+                            "6 - Fatores de um Número\n" +
+                            "7 - Estatísticas de Números\n" +
+                            "8 - Verificar Número Primo\n" +
+                            "9 - Calcular Fatorial\n" +
+                            "10 - Calcular MMC\n" +
+                            "11 - Calcular MDC\n" +
+                            "12 - Sequência de Fibonacci\n" +
+                            "13 - Progressão Aritmética (PA)\n" +
+                            "14 - Soma de PG\n" +
+                            "15 - Tabuada de 1 a 10\n" +
+                            "16 - Verificar Número Perfeito\n" +
+                            "17 - Sair\n\n" +
+                            "Selecione uma opção:"));
 
             switch (op) {
                 case 1 -> exe1();
@@ -158,6 +268,19 @@ public class main {
                 case 3 -> exe3();
                 case 4 -> exe4();
                 case 5 -> exe5();
+                case 6 -> exe6(); 
+                case 7 -> exe7();
+                /*
+                 * case 8 -> exe8();
+                 * case 9 -> exe9();
+                 * case 10 -> exe10();
+                 * case 11 -> exe11();
+                 * case 12 -> exe12();
+                 * case 13 -> exe13();
+                 * case 14 -> exe14();
+                 * case 15 -> exe15();
+                 * case 16 -> exe16();
+                 */
             }
         } while (op != 17);
     }
