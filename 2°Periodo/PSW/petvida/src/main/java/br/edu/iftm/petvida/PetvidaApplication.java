bf4c4ccc951b043cb7508011dc8d738526a8a785
Ghost <161037531+matt-carvalho117@users.noreply.github.com>
2026-09-10 20:48:10 -0300

    parte-a: sistema funcionando

---
+++ 2°Periodo/PSW/petvida/src/main/java/br/edu/iftm/petvida/PetvidaApplication.java
@@ -0,0 +1,29 @@
+package br.edu.iftm.petvida;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PetvidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetvidaApplication.class, args);
+	}
+
+	@Bean
+    public CommandLineRunner initData(TutorRepository tutorRepo, AnimalRepository animalRepo) {
+        return args -> {
+            // 1. Cadastrar os tutores
+            Tutor t1 = new Tutor(117, "Matt", "(11) 98888-1111");          
+            tutorRepo.salvar(t1);
+         
+            // 2. Cadastrar os animais associados aos tutores
+            Animal a1 = new Animal(47, "Mynerva", "Cachorro", 4, t1);    
+            animalRepo.salvar(a1);
+            
+            System.out.println("Tabelas criadas pelo schema.sql e dados inseridos via JDBC API!");
+        };
+	}
+}
