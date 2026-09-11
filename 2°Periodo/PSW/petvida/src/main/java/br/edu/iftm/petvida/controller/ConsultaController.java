000fc59696878f05f67c3a2f1e7aedec0239e6ac
Ghost <161037531+matt-carvalho117@users.noreply.github.com>
2026-09-10 21:40:29 -0300

    parte-d: defeitos corrigidos

--- 2°Periodo/PSW/petvida/src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
+++ 2°Periodo/PSW/petvida/src/main/java/br/edu/iftm/petvida/controller/ConsultaController.java
@@ -1,11 +1,13 @@
 package br.edu.iftm.petvida.controller;
  
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.edu.iftm.petvida.repository.AnimalRepository;
- 
+
+@Controller 
 public class ConsultaController {                          // <== ITEM 1
  
     @Autowired
@@ -13,7 +15,7 @@ public class ConsultaController {                          // <== ITEM 1
  
     @GetMapping("consulta")                                // <== ITEM 2
     public String consulta(Model model) {
-        model.addAttribute("bicho", repository.buscarPorId(1));   // <== ITEM 3
-        return "consulta.html";                            // <== ITEM 4
+        model.addAttribute("animal", repository.buscarPorId(47));   // <== ITEM 3
+        return "consulta";                            // <== ITEM 4
     }
 }
